3c918c0226cb48373a96c441a16a52f5ec150037
XamLua <black.hornetnikita@gmail.com>
2019-11-01 16:37:15 +0300

    4\13

--- OSP/l5/alphabet.c
+++ OSP/l5/alphabet.c
@@ -15,13 +15,177 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <pthread.h>
 
-int done = 0;
+
+static int done = 0;
+
+static char* alphabet;
 
 int main(int argc, char const *argv[])
 {
-	
+	signal(SIGINT, handle_sigint);
+
+	//Init the alphabet
+	alphabet = malloc(sizeof(char) * BUF_SIZE);
+	for (int i = 0; i < BUF_SIZE; ++i)
+		alphabet[i] = 97 + i;
+
+	if (argc < 2)
+		use_unnamed_semaphore();
+	else
+	{
+		int mode = (int) *argv[1];
+
+		switch(mode)
+		{
+			case 'u': 
+				use_unnamed_semaphore();
+				break;
+
+			case 'n':
+				break;
+
+			case 'm':
+				break;
+
+			default:
+				printf("Mode not supported\n");
+				return -1;
+		}
+	}
 	return 0;
+}
+
+void use_unnamed_semaphore()
+{
+	pthread_t invert_case_thread, invert_order_thread;
+
+	//Allocate memory for semaphores
+	sem_t *sem_c = malloc(2*sizeof(sem_t));
+	sem_t *sem_o = malloc(2*sizeof(sem_t));
+
+	//Initialize semaphores
+	if(sem_init(&sem_c[0], 0, 0) < 0)
+		crit_err(errno);
+
+	if(sem_init(&sem_o[0], 0, 0) < 0)
+		crit_err(errno);
+
+	if(sem_init(&sem_c[1], 0, 0) < 0)
+		crit_err(errno);
+
+	if(sem_init(&sem_o[1], 0, 0) < 0)
+		crit_err(errno);
+
+	//Create threads
+	if (pthread_create(&invert_order_thread, NULL, invert_case_unnamed, sem_c) < 0)
+		crit_err(errno);
+
+	if (pthread_create(&invert_case_thread, NULL, invert_order_unnamed, sem_o) < 0)
+		crit_err(errno);
+
+	//Main cycle
+	while(!done)
+	{
+		sleep(1);
+
+		//Wake up the 1'st thread
+		sem_post(&sem_c[0]);
+
+		//Wait until it's finished
+		sem_wait(&sem_c[1]);
+
+		//Print alphabet
+		printf("%.26s\n", alphabet);
+
+		sleep(1);
+
+		//Wake up the 2'nd thread
+		sem_post(&sem_o[0]);
+
+		//Wait until it's finished
+		sem_wait(&sem_o[1]);
+
+		//Print alphabet
+		printf("%.26s\n", alphabet);
+	}
+
+	//Free resources
+	sem_destroy(&sem_c[0]);
+	sem_destroy(&sem_o[0]);
+	sem_destroy(&sem_c[1]);
+	sem_destroy(&sem_o[1]);
+	free(sem_c);
+	free(sem_o);
+
+	return;
+}
+
+void *invert_case_unnamed(void *sem)
+{
+	sem_t *sem_c = (sem_t *) sem;
+
+	while(!done)
+	{
+		//Wait for semaphore
+		if(sem_wait(&sem_c[0]) < 0)
+			crit_err(errno);
+
+		invert_case(alphabet);
+
+		//Unlock semaphore for main thread
+		if(sem_post(&sem_c[1]) < 0)
+			crit_err(errno);
+	}
+
+	return NULL;
+}
+
+void *invert_order_unnamed(void *sem)
+{
+	sem_t *sem_o = (sem_t *) sem;
+
+	while(!done)
+	{
+		//Wait for semaphore
+		if(sem_wait(&sem_o[0]) < 0)
+			crit_err(errno);
+
+		invert_order(alphabet);
+
+		//Unlock semaphore for main thread
+		if(sem_post(&sem_o[1]) < 0)
+			crit_err(errno);
+	}
+
+	return NULL;
+}
+
+void invert_case(char* alphabet)
+{
+	for (int i = 0; i < BUF_SIZE; ++i)
+	{
+		if (alphabet[i] < 96)
+			alphabet[i] += 32;
+		else
+			alphabet[i] -= 32; 
+	}
+	return;
+}
+
+void invert_order(char* alphabet)
+{
+	char temp;
+
+	for (int i = 0; i < BUF_SIZE/2; ++i)
+	{
+		temp = alphabet[i];
+		alphabet[i] = alphabet[BUF_SIZE-1 - i];
+		alphabet[BUF_SIZE-1 - i] = temp;	
+	}
+
+	return;
 }
 
 void crit_err(int errnum)
@@ -33,5 +197,5 @@ void crit_err(int errnum)
 void handle_sigint(int sig)
 {
 	done = 1;
-	printf("Programm interrupted with '%s' signal\n", sys_siglist[sig]);
+	printf("Program interrupted with '%s' signal\n", sys_siglist[sig]);
 }
--- OSP/l5/alphabet.h
+++ OSP/l5/alphabet.h
@@ -1,6 +1,20 @@
 #ifndef __ALPH__
 #define __ALPH__
 
+#define BUF_SIZE 26
+
+void handle_sigint(int sig);
+
 void crit_err(int errnum);
+
+void invert_case(char* alphabet);
+
+void invert_order(char* alphabet);
+
+void use_unnamed_semaphore();
+
+void *invert_case_unnamed(void *sem);
+
+void *invert_order_unnamed(void *sem);
 
 #endif
