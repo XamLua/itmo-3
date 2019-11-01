8f864b631d1bab3b2de472236281972c56c77f4a
XamLua <black.hornetnikita@gmail.com>
2019-11-01 18:52:57 +0300

    5\13

--- OSP/l5/alphabet.c
+++ OSP/l5/alphabet.c
@@ -43,8 +43,8 @@ int main(int argc, char const *argv[])
 				use_unnamed_semaphore();
 				break;
 
-			case 'n':
-				break;
+			case 's':
+				use_s5_semaphore();
 
 			case 'm':
 				break;
@@ -54,7 +54,167 @@ int main(int argc, char const *argv[])
 				return -1;
 		}
 	}
+	free(alphabet);
 	return 0;
+}
+
+void use_s5_semaphore()
+{
+	pthread_t invert_case_thread, invert_order_thread;
+
+	int semid_c, semid_o;
+
+	//Get semaphore set for each thread
+	if ((semid_c = semget(SEM_ID_C, 2, PERMISSIONS | IPC_CREAT)) < 0)
+		crit_err(errno);
+	if ((semid_o = semget(SEM_ID_O, 2, PERMISSIONS | IPC_CREAT)) < 0)
+		crit_err(errno);
+	printf("semid_c = %d, semid_o = %d\n", semid_c, semid_o);
+
+	//Init semaphores values (blocked)
+	semctl(semid_c, 0, SETALL, 0);
+	semctl(semid_o, 0, SETALL, 0);
+
+	//Prepare buffer structs for operaions
+	struct sembuf sembuf_c = {.sem_num = 0, .sem_op = -1, .sem_flg = 0};
+	struct sembuf sembuf_o = {.sem_num = 0, .sem_op = -1, .sem_flg = 0};
+
+	//Create threads
+	if (pthread_create(&invert_case_thread, NULL, invert_case_s5, NULL) < 0)
+		crit_err(errno);
+
+	if (pthread_create(&invert_order_thread, NULL, invert_order_s5, NULL) < 0)
+		crit_err(errno);
+
+	//Main cycle
+	while(!done)
+	{
+		sleep(1);
+
+		//Wake up the 1'st thread
+		sembuf_c.sem_num = 0;		
+		sembuf_c.sem_op = 1;
+
+		if(semop(semid_c, &sembuf_c, 1) < 0)
+			crit_err(errno);
+
+		//Wait until it's finished
+		sembuf_c.sem_num = 1;
+		sembuf_c.sem_op = -1;
+
+		if(semop(semid_c, &sembuf_c, 1) < 0)
+			crit_err(errno);		
+
+		//Print alphabet
+		printf("%.26s\n", alphabet);
+
+		sleep(1);
+
+		//Wake up the 2'nd thread
+		sembuf_o.sem_num = 0;		
+		sembuf_o.sem_op = 1;
+
+		if(semop(semid_o, &sembuf_o, 1) < 0)
+			crit_err(errno);
+
+		//Wait until it's finished
+		sembuf_o.sem_num = 1;
+		sembuf_o.sem_op = -1;
+		if(semop(semid_o, &sembuf_o, 1) < 0)
+			crit_err(errno);
+
+		//Print alphabet
+		printf("%.26s\n", alphabet);
+	}
+
+	//Finish child threads
+	sembuf_c.sem_num = 0;		
+	sembuf_c.sem_op = 1;
+
+	if(semop(semid_c, &sembuf_c, 1) < 0)
+		crit_err(errno);
+	
+	sembuf_o.sem_num = 0;		
+	sembuf_o.sem_op = 1;
+
+	if(semop(semid_o, &sembuf_o, 1) < 0)
+		crit_err(errno);
+
+	pthread_join(invert_case_thread, NULL);
+	pthread_join(invert_order_thread, NULL);
+
+	//Free resources
+	if (semctl(semid_c, 0, IPC_RMID, 0) < 0)		
+		crit_err(errno);
+	if (semctl(semid_o, 0, IPC_RMID, 0) < 0)		
+		crit_err(errno);
+}
+
+void *invert_case_s5()
+{
+	int semid_c;
+	//Get semaphore set
+	if ( (semid_c = semget(SEM_ID_C, 2, PERMISSIONS)) < 0)
+		crit_err(errno);
+
+	printf("from case SEM_ID_C = %d, semid_c = %d\n", SEM_ID_C, semid_c);
+	//Prepare buffer for operations
+	struct sembuf sembuf_c = {.sem_num = 0, .sem_op = 0, .sem_flg = 0};
+
+	//Main cycle
+	while(!done)
+	{
+		//Wait for semaphore
+		sembuf_c.sem_num = 0;
+		sembuf_c.sem_op = -1;
+
+		if(semop(semid_c, &sembuf_c, 1) < 0)
+			crit_err(errno);
+
+		invert_case(alphabet);
+
+		//Wake up the main thread
+		sembuf_c.sem_num = 1;
+		sembuf_c.sem_op = 1;
+		
+		if(semop(semid_c, &sembuf_c, 1) < 0)
+			crit_err(errno);
+	}
+
+	return NULL;
+}
+
+void *invert_order_s5()
+{
+	int semid_o;
+	//Get semaphore set
+	if ( (semid_o = semget(SEM_ID_O, 2, PERMISSIONS)) < 0)
+		crit_err(errno);
+
+	printf("from order !SEM_ID_O! = %d, semid_o = %d\n", SEM_ID_O, semid_o);
+	//Prepare buffer for operations
+	struct sembuf sembuf_o = {.sem_num = 0, .sem_op = 0, .sem_flg = 0};
+
+	//Main cycle
+	while(!done)
+	{
+		//Wait for semaphore
+		sembuf_o.sem_num = 0;
+		sembuf_o.sem_op = -1;
+
+		if(semop(semid_o, &sembuf_o, 1) < 0)
+			crit_err(errno);
+
+		invert_order(alphabet);
+
+		//Wake up the main thread
+		sembuf_o.sem_num = 1;
+		sembuf_o.sem_op = 1;
+		if(semop(semid_o, &sembuf_o, 1) < 0)
+			crit_err(errno);
+	}
+
+	return NULL;
 }
 
 void use_unnamed_semaphore()
@@ -91,10 +251,15 @@ void use_unnamed_semaphore()
 		sleep(1);
 
 		//Wake up the 1'st thread
-		sem_post(&sem_c[0]);
+		if(sem_post(&sem_c[0]) < 0)
+		{
+			printf("suka\n");
+			crit_err(errno);
+		}
 
 		//Wait until it's finished
-		sem_wait(&sem_c[1]);
+		if(sem_wait(&sem_c[1]) < 0)
+			crit_err(errno);
 
 		//Print alphabet
 		printf("%.26s\n", alphabet);
@@ -102,14 +267,26 @@ void use_unnamed_semaphore()
 		sleep(1);
 
 		//Wake up the 2'nd thread
-		sem_post(&sem_o[0]);
+		if(sem_post(&sem_o[0]) < 0)
+			crit_err(errno);
 
 		//Wait until it's finished
-		sem_wait(&sem_o[1]);
+		if(sem_wait(&sem_o[1]) < 0)
+			crit_err(errno);
 
 		//Print alphabet
 		printf("%.26s\n", alphabet);
 	}
+
+	//Finish child threads
+	if(sem_post(&sem_c[0]) < 0)
+		crit_err(errno);
+
+	if(sem_post(&sem_o[0]) < 0)
+		crit_err(errno);
+
+	pthread_join(invert_case_thread, NULL);
+	pthread_join(invert_order_thread, NULL);
 
 	//Free resources
 	sem_destroy(&sem_c[0]);
--- OSP/l5/alphabet.h
+++ OSP/l5/alphabet.h
@@ -3,6 +3,11 @@
 
 #define BUF_SIZE 26
 
+#define SEM_ID_C 313
+#define SEM_ID_O 626
+
+#define PERMISSIONS 0666
+
 void handle_sigint(int sig);
 
 void crit_err(int errnum);
@@ -16,5 +21,11 @@ void use_unnamed_semaphore();
 void *invert_case_unnamed(void *sem);
 
 void *invert_order_unnamed(void *sem);
+
+void use_s5_semaphore();
+
+void *invert_case_s5();
+
+void *invert_order_s5();
 
 #endif
